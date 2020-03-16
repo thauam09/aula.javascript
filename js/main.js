d814269eb3b1545ef0bb9f2ec14900f8981ec0db
thauã martins <thauam_09martins@hotmail.com>
2020-03-16 19:35:41 -0300

    Primeiro Commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar<br>:D";
+}
+
+
+
+/*
+var nome = "Thauã Martins";
+var idade = 29;
+alert("Meu primeiro JS: " + nome + " de " + idade + " anos");
+
+
+var lista = ["maçã", "pera", "laranja"];
+
+console.log(lista);
+lista.push("Abacate");//Adiciona item ao final da lista
+lista.pop(); //Remove último item da lista
+lista.reverse // Traz os itens da lista na ordem contrária
+lista.join(" - "); //Transforma a array em String, separando os items pelo que for declarado na função
+
+//Dicionário
+var frutas = [{nome:"maçã", cor:"Vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+
+var idade = prompt("Qual sua idade?");
+*/
